28bf4b0565e8fc17ddec5752ace2d50553795b7c
Shashank Naik <shashanknaik808@gmail.com>
2023-12-06 14:39:47 +0530

    Created a basic grid layout with a card and lizard content

--- my-app/src/Layout.jsx
+++ my-app/src/Layout.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import {
+    Card,
+    CardActionArea,
+    CardMedia,
+    Grid,
+    Typography
+} from '@mui/material';
 
 function Layout() {
     return (
-        <div>Layout</div>
+        <div>
+            <Grid container spacing={3}>
+                <Grid item xs={3}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image="/static/images/cards/contemplative-reptile.jpg"
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards were a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
