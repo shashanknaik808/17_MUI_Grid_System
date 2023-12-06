0c87f27ac4844004065d5e64508e31d51bb7eb1a
Shashank Naik <shashanknaik808@gmail.com>
2023-12-06 15:09:30 +0530

    Added multiple grid items with IGNEEL content in Layout component

--- my-app/src/Layout.jsx
+++ my-app/src/Layout.jsx
@@ -19,7 +19,85 @@ function Layout() {
                         <CardActionArea>
                             <CardMedia
                                 component="img"
-                                height="140"
+                                height="100%"
+                                image="/images/igneel.jpg"
+                                alt="Fire Dragon King 'IGNEEL'"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    IGNEEL
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Igneel is a male fire dragon and the adoptive father of Dragon Slayer Natsu Dragneel, a wizard of the guild Fairy Tail. He is the king of the Fire Dragons and a friend of fellow fire dragon Atlas Flame. He was also the biological father of Ignia, the Fire God Dragon.
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                <Grid item xs={3} md={2.4} lg={2}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="100%"
+                                image="/images/igneel.jpg"
+                                alt="Fire Dragon King 'IGNEEL'"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    IGNEEL
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Igneel is a male fire dragon and the adoptive father of Dragon Slayer Natsu Dragneel, a wizard of the guild Fairy Tail. He is the king of the Fire Dragons and a friend of fellow fire dragon Atlas Flame. He was also the biological father of Ignia, the Fire God Dragon.
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                <Grid item xs={3} md={2.4} lg={2}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="100%"
+                                image="/images/igneel.jpg"
+                                alt="Fire Dragon King 'IGNEEL'"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    IGNEEL
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Igneel is a male fire dragon and the adoptive father of Dragon Slayer Natsu Dragneel, a wizard of the guild Fairy Tail. He is the king of the Fire Dragons and a friend of fellow fire dragon Atlas Flame. He was also the biological father of Ignia, the Fire God Dragon.
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                <Grid item xs={3} md={2.4} lg={2}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="100%"
                                 image="/images/igneel.jpg"
                                 alt="Fire Dragon King 'IGNEEL'"
                             />
