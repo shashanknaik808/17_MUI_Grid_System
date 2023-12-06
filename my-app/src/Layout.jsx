923702c1887f742e28548d34fa5d6371553280df
Shashank Naik <shashanknaik808@gmail.com>
2023-12-06 15:05:09 +0530

    Included Igneel in the Layout component

--- my-app/src/Layout.jsx
+++ my-app/src/Layout.jsx
@@ -14,7 +14,7 @@ function Layout() {
     return (
         <div>
             <Grid container spacing={3}>
-                <Grid item xs={3}>
+                <Grid item xs={3} md={2.4} lg={2}>
                     <Card sx={{ maxWidth: 345 }}>
                         <CardActionArea>
                             <CardMedia
@@ -29,19 +29,17 @@ function Layout() {
                                 </Typography>
                                 <Typography variant="body2" color="text.secondary">
                                     Igneel is a male fire dragon and the adoptive father of Dragon Slayer Natsu Dragneel, a wizard of the guild Fairy Tail. He is the king of the Fire Dragons and a friend of fellow fire dragon Atlas Flame. He was also the biological father of Ignia, the Fire God Dragon.
-
                                 </Typography>
                             </CardContent>
                         </CardActionArea>
-
                         <CardActions>
                             <Button size="small" color="primary">
-                                Shared
+                                Share
                             </Button>
                         </CardActions>
-
                     </Card>
                 </Grid>
+
             </Grid>
         </div>
     )
